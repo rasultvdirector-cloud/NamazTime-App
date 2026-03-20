1c7c9213eae955ef2ac4d4a559c4b27a7ef43c27
Rasul Alakbar <rasul.tvdirector@gmail.com>
2026-03-20 23:41:02 +0400

    Release version 1.0.7 with telemetry endpoint

--- android_app/app/build.gradle.kts
+++ android_app/app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "com.muslimtime.app"
         minSdk = 24
         targetSdk = 35
-        versionCode = 8
-        versionName = "1.0.6"
+        versionCode = 9
+        versionName = "1.0.7"
         buildConfigField("String", "TELEMETRY_BASE_URL", "\"$telemetryBaseUrl\"")
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
